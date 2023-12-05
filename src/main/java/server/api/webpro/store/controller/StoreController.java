30fd9d50931eeeb7fae200e364a531339381064c
wodnd0131 <wodnd0131@naver.com>
2023-12-06 01:42:00 +0900

    feat:loginAdditional

--- src/main/java/server/api/webpro/store/controller/StoreController.java
+++ src/main/java/server/api/webpro/store/controller/StoreController.java
@@ -1,11 +1,10 @@
-package server.api.webpro.store.control;
+package server.api.webpro.store.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
-import server.api.webpro.review.service.ReviewService;
 
 @RestController
 @RequiredArgsConstructor
-public class StoreControl {
+public class StoreController {
 //    private final StoreControl storeControl;
 }
