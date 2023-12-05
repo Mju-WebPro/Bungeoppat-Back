fe27df51a69005b809e1a0fc7d73ec8b0b5eb54f
JongHyeon <choijonghyen@gmail.com>
2023-12-05 12:06:16 +0900

    feat: payment 기능 구현

--- src/main/java/server/api/webpro/payment/entity/Payment.java
+++ src/main/java/server/api/webpro/payment/entity/Payment.java
@@ -1,18 +1,32 @@
 package server.api.webpro.payment.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "Payment")
 public class Payment {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "payment_id")
     private Long id;
-
-    @Column(name = "amount", nullable = false)
-    private Integer amount;
+    @Column(name = "paid_amount", nullable = false)
+    private Long paidAmount;
+    @Column(name = "pay_method", nullable = false)
+    private String payMethod;
+    @Column(name = "status", nullable = false)
+    private String status;
     @Column(name = "imp_uid", nullable = false)
-    private String imp_uid;
+    private String impUid;
+
+    @Builder
+    public Payment(Long paidAmount, String payMethod, String status, String impUid){
+        this.paidAmount = paidAmount;
+        this.payMethod = payMethod;
+        this.status = status;
+        this.impUid = impUid;
+    }
 }
